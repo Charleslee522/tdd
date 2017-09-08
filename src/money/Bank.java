af0af18403bc3b189db0eeb3bea653bbea2573a5
charles <charleslee522@gmail.com>
2017-09-08 22:39:07 +0900

    add rate to Bank

--- src/money/Bank.java
+++ src/money/Bank.java
@@ -3,11 +3,18 @@ package money;
 import expression.Expression;
 
 public class Bank {
+	
+	private int rate;
+	
 	Money reduce(Expression source, String to) {
 		return source.reduce(this, to);
 	}
 	
 	public void addRate(String from, String to, int rate) {
-		
+		this.rate = rate;
+	}
+	
+	public int rate(String from, String to) {
+		return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
 	}
 }
