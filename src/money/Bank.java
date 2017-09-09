9b3d8176bc48c341fb82b2a4e1737624c9b1e6f4
charles <charleslee522@gmail.com>
2017-09-09 09:04:08 +0900

    add HashTable class

--- src/money/Bank.java
+++ src/money/Bank.java
@@ -1,16 +1,21 @@
 package money;
 
+import java.util.Hashtable;
+
 import expression.Expression;
+import javafx.util.Pair;
 
 public class Bank {
 	
 	private int rate;
+	private Hashtable<Pair<String, String>, Integer> rates = new Hashtable<Pair<String, String>, Integer>();
 	
 	Money reduce(Expression source, String to) {
 		return source.reduce(this, to);
 	}
 	
 	public void addRate(String from, String to, int rate) {
+		rates.put(new Pair(from, to), new Integer(rate));
 		this.rate = rate;
 	}
 	
